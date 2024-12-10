8fc0511242cc644b55c4e909f9800b64c4c4249d
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2024-12-10 18:12:36 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,10 +25,10 @@ export default async function Home() {
         <div className="ml-3 h-screen grid grid-cols-[65%_35%] flex justify-center items-start">
           <div>
             <div className="flex text-sm mb-10 border-b">
-              <div className="m-3">
+              <div className="m-3 text-slate-500 hover:text-slate-900">
                 <button>Follow</button>
               </div>
-              <div className="m-3">
+              <div className="m-3 text-slate-500 hover:text-slate-900">
                 <button>Following</button>
               </div>
             </div>
@@ -36,49 +36,49 @@ export default async function Home() {
               <div className="text-xl font-bold">
                 This is the title of the content
               </div>
-              <div className="text-md text-slate-600">
+              <div className="text-md text-slate-500">
                 This is the actual content, where posts will appear.This is the actual content, where posts will appear 
               </div>
               <br />
               <div className="text-xl font-bold">
                 This is the title of the content
               </div>
-              <div className="text-md text-slate-600">
+              <div className="text-md text-slate-500">
                 This is the actual content, where posts will appear.This is the actual content, where posts will appear 
               </div>
               <br />
               <div className="text-xl font-bold">
                 This is the title of the content
               </div>
-              <div className="text-md text-slate-600">
+              <div className="text-md text-slate-500">
                 This is the actual content, where posts will appear.This is the actual content, where posts will appear 
               </div>
               <br />
               <div className="text-xl font-bold">
                 This is the title of the content
               </div>
-              <div className="text-md text-slate-600">
+              <div className="text-md text-slate-500">
                 This is the actual content, where posts will appear.This is the actual content, where posts will appear 
               </div>
               <br />
               <div className="text-xl font-bold">
                 This is the title of the content
               </div>
-              <div className="text-md text-slate-600">
+              <div className="text-md text-slate-500">
                 This is the actual content, where posts will appear.This is the actual content, where posts will appear 
               </div>
               <br />
               <div className="text-xl font-bold">
                 This is the title of the content
               </div>
-              <div className="text-md text-slate-600">
+              <div className="text-md text-slate-500">
                 This is the actual content, where posts will appear.This is the actual content, where posts will appear 
               </div>
               <br />
               <div className="text-xl font-bold">
                 This is the title of the content
               </div>
-              <div className="text-md text-slate-600">
+              <div className="text-md text-slate-500">
                 This is the actual content, where posts will appear.This is the actual content, where posts will appear 
               </div>
               <br />
@@ -87,43 +87,45 @@ export default async function Home() {
 
           <div className="text-md h-screen border-l p-5">
             <div className="mb-8">
-              <h2 className="text-lg font-semibold mb-4">Suggested Blogs</h2>
-              <ul>
-                <li className="text-md mb-2">
-                  <a href="#" className="text-slate-900 hover:text-slate-500">How to Become a Better Writer</a>
-                  <div>
-                    <div className="text-sm text-slate-500">
-                      Becoming better writer requires alot of practice and patience. It's not something that can be done overnight. But it's something that you can do everyday.
+              <h2 className="text-md font-semibold mb-4">Staf Picks</h2>
+              <div className="m-2">
+                <ul>
+                  <li className="text-md mb-4">
+                    <a className="text-slate-900 hover:text-slate-500 font-bold hover:cursor-pointer">How to Become a Better Writer</a>
+                    <div>
+                      <div className="text-sm text-slate-500">
+                        Becoming better writer requires alot of practice and patience. It's not something that can be done overnight. But it's something that you can do everyday.
+                      </div>
                     </div>
-                  </div>
-                </li>
+                  </li>
 
-                <li className="text-md mb-2">
-                  <a href="#" className="text-slate-900 hover:text-slate-500">A tip for Effective Remote Work</a>
-                  <div className="text-sm text-slate-500">
-                    Communincate with your team members and clients through video conferencing. This will help you to build better relationships with them.
-                  </div>
-                </li>
+                  <li className="text-md mb-4">
+                    <a className="text-slate-900 hover:text-slate-500 font-bold hover:cursor-pointer">A tip for Effective Remote Work</a>
+                    <div className="text-sm text-slate-500">
+                      Communincate with your team members and clients through video conferencing. This will help you to build better relationships with them.
+                    </div>
+                  </li>
 
-                <li className="text-md mb-2">
-                  <a href="#" className="text-slate-900 hover:text-slate-500">Mastering JavaScript in 2024</a>
-                  <div className="text-sm text-slate-500">
-                    Mastering JavaScript in 2024 is a challenging but rewarding task. It requires a lot of practice and patience. But with the right mindset and approach, you can achieve it.
-                  </div>
-                </li>
-              </ul>
+                  <li className="text-md mb-4">
+                    <a className="text-slate-900 hover:text-slate-500 font-bold hover:cursor-pointer">Mastering JavaScript in 2024</a>
+                    <div className="text-sm text-slate-500">
+                      Mastering JavaScript in 2024 is a challenging but rewarding task. It requires a lot of practice and patience. But with the right mindset and approach, you can achieve it.
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              
               
             </div>
             <div className="mb-8">
-              <h2 className="text-lg font-bold mb-4">Recommended Topics</h2>
-              <ul className="flex flex-wrap gap-2">
-                <li className="bg-gray-900 px-3 py-1 rounded hover:text-slate-500 text-white">Technology</li>
-                <li className="bg-gray-900 px-3 py-1 rounded hover:text-slate-500 text-white">Health</li>
-                <li className="bg-gray-900 px-3 py-1 rounded hover:text-slate-500 text-white">Writing</li>
-                <li className="bg-gray-900 px-3 py-1 rounded hover:text-slate-500 text-white">Productivity</li>
-              </ul>
+              <h2 className="text-md font-bold mb-4">Recommended Topics</h2>
+                  <ul className="flex flex-wrap gap-2">
+                  <li className="bg-gray-100 px-4 py-2 rounded-3xl hover:text-slate-500 text-slate-900 text-sm">Technology</li>
+                  <li className="bg-gray-100 px-4 py-2 rounded-3xl hover:text-slate-500 text-slate-900 text-sm">Programming</li>
+                  <li className="bg-gray-100 px-4 py-2 rounded-3xl hover:text-slate-500 text-slate-900 text-sm">Science</li>
+                  <li className="bg-gray-100 px-4 py-2 rounded-3xl hover:text-slate-500 text-slate-900 text-sm">Productivity</li>
+                </ul>
             </div>
-
             <div>
               <h2 className="text-lg font-bold mb-4">Who to Follow</h2>
               <ul>
@@ -135,8 +137,11 @@ export default async function Home() {
                     className="rounded-full mr-3"
                   />
                   <div>
-                    <div className="font-bold">Riken Patel</div>
-                    <button className="text-slate-900 hover:text-slate-500">Follow</button>
+                    <div className="font-bold text-md">Riken Patel</div>
+                    <div className="text-sm text-slate-500">
+                      A famous trader and coder. The best in business and billionaire.
+                    </div>
+                    <button className="px-4 py-2 rounded-3xl hover:text-black text-slate-900 text-sm border border-black">Follow</button>
                   </div>
                 </li>
 
@@ -149,7 +154,10 @@ export default async function Home() {
                   />
                   <div>
                     <div className="font-bold">Anonymus</div>
-                    <button className="text-slate-900 hover:text-slate-500">Follow</button>
+                    <div className="text-sm text-slate-500">
+                      An anonymus person from planet earth.
+                    </div>
+                    <button className="px-4 py-2 rounded-3xl hover:text-black text-slate-900 text-sm border border-black">Follow</button>
                   </div>
                 </li>
 
@@ -162,7 +170,10 @@ export default async function Home() {
                   />
                   <div>
                     <div className="font-bold">Anonymus</div>
-                    <button className="text-slate-900 hover:text-slate-500">Follow</button>
+                    <div className="text-sm text-slate-500">
+                      An anonymus person from planet earth.
+                    </div>
+                    <button className="px-4 py-2 rounded-3xl hover:text-black text-slate-900 text-sm border border-black">Follow</button>
                   </div>
                 </li>
               </ul>
@@ -173,7 +184,5 @@ export default async function Home() {
     </div>
   );
 }
-
-
 
 
