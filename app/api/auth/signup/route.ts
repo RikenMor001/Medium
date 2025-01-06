3afce8f599b755366316357b66fdf508b69a496b
Riken Patel <149461465+RikenMor001@users.noreply.github.com>
2025-01-05 23:29:48 -0500

    Update route.ts

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,51 +1,34 @@
-import { PrismaClient } from "@prisma/client";
+
 import { NextRequest, NextResponse } from "next/server";
-import  bcrypt  from "bcrypt"
-import jwt from "jsonwebtoken"
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
-const JWT_SECRET = process.env.JWT_SECRET || "kingRiken"
+export default async function POST(req:NextRequest){
+    const [ username, password ] = await req.json();
+    const user = await prisma.user.findUnique({
+        where: {
+            username: username
+        }
+    })   
 
-export async function POST(req: NextRequest){
-    try {
-        const { username, password } = await req.json();
-    
-        if (!username || !password){
-            NextResponse.json({
-                error: "Invalid username or password"
-            }, { status: 400 })
-        }
-        const existsingUser = await prisma.user.findUnique({
-            where: {
-                username: username
-            }
-        })
-        
-        if (existsingUser){
-            NextResponse.json({
-                error: "Invalid username or password"
-            }, { status: 400 })
-        }
-    
-        const hashingPassword = await bcrypt.hash(password, 10);
-    
-        const newUser = await prisma.user.create({
-            data: {
-                username: username,
-                password: hashingPassword
-            }
-        })
-    
-        const jwtToken = jwt.sign({
-            id: newUser.id
-        }, JWT_SECRET)
-        return NextResponse.json({
-            message: "User created successfully!", jwtToken 
-        })
-    }
-    catch(error: any){
+    if( user ){
         NextResponse.json({
-            error: "There was an error while doing this process"
-        }, {status: 403})
+            error: "User already exists."
+        })
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+        data: {
+            username,
+            password: hashedPassword
+        }
+    })
+
+    NextResponse.json({
+        message: "User createed successfully."
+    })
+
 }
